c108405568de1febeefee82e0ea4ae9ef3932e8b
Simone Torres <simonebezerra.torres@gmail.com>
2025-03-12 14:22:21 -0300

    Aventureiro CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+Adicionado cálculo de densidade populacional e PIB per capita
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
